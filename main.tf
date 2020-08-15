1a638bbe2941a4f34ad45d7c485e1724046aaf54
Anton Ovinnikov <toshakins@gmail.com>
2020-08-15 14:51:19 +0300

    need port 80 for certbot

--- main.tf
+++ main.tf
@@ -83,6 +83,13 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     protocol    = "-1"
