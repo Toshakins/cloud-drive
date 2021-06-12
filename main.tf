e3561501d190a343582b181df4b4d93f2e1f8053
Anton Ovinnikov <toshakins@gmail.com>
2021-06-12 17:47:02 +0300

    upgrade terraform version

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 0.12"
+  required_version = "~> 1.0"
   backend "s3" {
     bucket         = "terraform-aws-init-bucket"
     key            = "cloudform/terraform.tfstate"
